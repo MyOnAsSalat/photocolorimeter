9eff4f51638387d5b47fa7d412648d873a7b15e4
MyOnAsSalat <deniska.xrenov23@yandex.ru>
2017-11-19 15:40:47 +0800

    Added arduino code

---
+++ ArduinoComportContoller/ArduinoComportContoller.ino
@@ -0,0 +1,20 @@
+
+void setup() 
+{
+  Serial.begin(9600); 
+
+}
+double i = 0;
+void loop() 
+{
+  while (Serial.available() > 0) 
+  {
+      if (Serial.read() == 83 )
+      {
+        Serial.println(82);
+        break;
+      }
+      Serial.println(sin(i)*100, 7);
+      i = i + 0.08d;
+  }
+}
